42855f2ac25de41284a58a872d02228e95e59a22
Nathan Brewer <nathan.a.brewer@dftz.org>
2015-07-03 13:19:11 -0600

    log warnings/info

--- vlc/node-red-contrib-vlc.js
+++ vlc/node-red-contrib-vlc.js
@@ -10,7 +10,6 @@ module.exports = function(RED) {
         // Create a RED node
         RED.nodes.createNode(this,config);
         var node = this;
-
         var vlcRequest = function(params, deferred){
           var p = '/requests/status.json?n=b';
           for( var field in params )
@@ -25,29 +24,22 @@ module.exports = function(RED) {
               'Content-Type' : 'application/json'
             }
           };
-
           var req = http.request(reqOptions, function (response) {
                var buffer = '';
                response.setEncoding('utf8');
                response.on('data', function (chunk) {
                  buffer += chunk;
                });
-
                response.on('end', function() {
                  var result = JSON.parse(buffer);
                  node.lastResponse = result;
-
-                 deferred.resolve(node, result);
-
+                 deferred.resolve(result);
                });
           });
-
           req.on('error', function(err) {
-              deferred.reject(node, err);
+              deferred.reject(err);
           });
-
           req.end();
-
         };
 
         //Actual Input
@@ -98,6 +90,7 @@ module.exports = function(RED) {
         this.inPlay = function(uri){
           var deferred = Q.defer();
           var params = {command:'in_play', input: uri};
+          node.warn("Loading "+uri);
           vlcRequest(params,deferred);
 
           return deferred.promise;
@@ -106,6 +99,7 @@ module.exports = function(RED) {
         this.stop = function(cb){
           var deferred = Q.defer();
           var params = {command:'pl_stop'};
+          node.warn("Stop ");
           vlcRequest(params,deferred);
 
           return deferred.promise;
@@ -115,6 +109,7 @@ module.exports = function(RED) {
           var deferred = Q.defer();
           var params = {command:'pl_pause'};
           if(id) params.id = id;
+          node.warn("Pause ");
           vlcRequest(params,deferred);
 
           return deferred.promise;
@@ -123,6 +118,7 @@ module.exports = function(RED) {
         this.seek = function(position){
           var deferred = Q.defer();
           var params = {command:'seek', val: position};
+          node.warn("[nrVLC]: Seek "+position);
           vlcRequest(params,deferred);
 
           return deferred.promise;
@@ -133,21 +129,17 @@ module.exports = function(RED) {
           var params = {command:'fullscreen'};
 
           if(node.lastResponse.fullscreen != reqFullScreen){
+            node.warn("Fullscreen request ");
             vlcRequest(params,deferred);
           }else{
             //just for the sake of returning this promise
+            node.warn("Fullscreen, already in requested state, ignoring ");
             setTimeout(function(){deferred.resolve();}, 100);
           }
           return deferred.promise;
 
         }
 
-        this.wait = function(time){
-          var deferred = Q.defer();
-          setTimeout(function(){ deffered.resolve(); }(deferred),time);
-
-         return deferred.promise;
-        }
     }
 
 
